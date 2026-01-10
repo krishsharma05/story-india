3f9c9db178313126682544b8955871088eb90e8c
Krish Sharma <ks9996200@gmail.com>
2026-01-10 21:37:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-function openPage(pageId){
-  document.querySelectorAll('.page').forEach(p=>{
-    p.classList.remove('active');
-  });
-  document.getElementById(pageId).classList.add('active');
-}
+const container = document.querySelector(".reels-container");
+const videos = document.querySelectorAll(".reel video");
 
-const videos = document.querySelectorAll("video");
+/* Play first reel */
+videos[0].play().catch(()=>{});
 
-/* Sound ON by default */
-videos.forEach(video => {
-  video.muted = false;
+/* Scroll autoplay */
+container.addEventListener("scroll", () => {
+  videos.forEach(video => {
+    const rect = video.getBoundingClientRect();
 
-  // TAP to Play / Pause
-  video.addEventListener("click", () => {
-    if (video.paused) {
-      video.play();
+    if(rect.top >= 0 && rect.bottom <= window.innerHeight + 20){
+      video.play().catch(()=>{});
     } else {
       video.pause();
     }
   });
 });
 
-/* Scroll par sirf visible reel chale */
-document.querySelector('.reels-container').addEventListener('scroll', () => {
-  videos.forEach(video => {
-    const rect = video.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+/* Tap to play / pause */
+videos.forEach(video => {
+  video.addEventListener("click", () => {
+    if(video.paused){
       video.play();
     } else {
       video.pause();
